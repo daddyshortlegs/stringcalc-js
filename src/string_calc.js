d13d5bada3fcf5b95ecef019e0661aed9645df08
Andy Smith <andysmith836@gmail.com>
2019-08-08 22:57:49 +0100

    Break down into smaller functions

--- src/string_calc.js
+++ src/string_calc.js
@@ -5,27 +5,38 @@ function add(theString) {
         return 0;
     }
 
-    const strings = splitValuesByDelimiter(theString);
-    throwErrorIfNegatives(strings);
-    const filteredNumbers = filterNumbers(strings);
+    return totalUpRelevantNumbers(theString);
+}
+
+function totalUpRelevantNumbers(theString) {
+    const numbers = splitNumbersFromInput(theString);
+    throwErrorIfNegatives(numbers);
+    const filteredNumbers = filterNumbers(numbers);
     return sumValues(filteredNumbers);
 }
 
-function throwErrorIfNegatives(strings) {
-    let negatives = strings.map(s => parseInt(s)).filter(i => i < 0);
+function splitNumbersFromInput(theString) {
+    const strings = splitValuesByDelimiter(theString);
+    return toArrayOfInts(strings);
+}
+
+function throwErrorIfNegatives(numbers) {
+    let negatives = numbers.filter(i => i < 0);
     if (negatives.length > 0) {
         throw "error: negatives not allowed:" + negatives.reduce((acc, item) => acc + " " + item, "");
     }
 }
 
-function filterNumbers(strings) {
-    return strings.map(s => parseInt(s))
-        .filter(i => i < 1001);
+function filterNumbers(numbers) {
+    return numbers.filter(i => i < 1001);
 }
 
-function sumValues(strings) {
-    return strings.map(s => parseInt(s))
-        .reduce((total, acc) => total + acc);
+function sumValues(numbers) {
+    return numbers.reduce((total, acc) => total + acc);
+}
+
+function toArrayOfInts(strings) {
+    return strings.map(s => parseInt(s));
 }
 
 export {add}
