5f13ee75fe8c60cbd0ee7883ae992baba1d4c7ad
Andy Smith <andysmith836@gmail.com>
2019-08-08 21:39:38 +0100

    Sum numbers passed in

--- src/string_calc.js
+++ src/string_calc.js
@@ -2,7 +2,10 @@ function add(numbers) {
     if (numbers === "") {
         return 0;
     }
-    return parseInt(numbers);
+
+    let strings = numbers.split(",");
+    return strings.map(s => parseInt(s))
+        .reduce((total, acc) => total + acc);
 }
 
 export {add}
--- src/string_calc.spec.js
+++ src/string_calc.spec.js
@@ -1,14 +1,15 @@
-import { add } from "./string_calc";
+import {add} from "./string_calc";
 
 describe('just testing', function() {
     it('should return 0 for empty string', function() {
-        const result = add("");
-        expect(result).toEqual(0);
+        expect(add("")).toEqual(0);
     });
 
     it('should return 4 when passed in', function() {
-        const result = add("4");
-        expect(result).toEqual(4);
+        expect(add("4")).toEqual(4);
     });
 
+    it('should sum 2 numbers', function() {
+        expect(add("1,2")).toEqual(3);
+    });
 });
