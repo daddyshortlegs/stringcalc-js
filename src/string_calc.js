1e3e7f7a283d1ea96d0e5090bfc0ae5231591044
Andy Smith <andysmith836@gmail.com>
2019-08-08 21:55:12 +0100

    Get custom separator working, bit of a longer period between commits :-(

--- src/string_calc.js
+++ src/string_calc.js
@@ -3,7 +3,17 @@ function add(numbers) {
         return 0;
     }
 
-    let strings = numbers.split(",");
+    let regExp = RegExp(/\/\/(.*)\n(.*)/);
+    let matches = regExp.exec(numbers);
+    let separator;
+    if (matches !== null) {
+        separator = matches[1];
+        numbers = matches[2];
+    } else {
+        separator = /,|\n/
+    }
+
+    let strings = numbers.split(separator);
     return strings.map(s => parseInt(s))
         .reduce((total, acc) => total + acc);
 }
--- src/string_calc.spec.js
+++ src/string_calc.spec.js
@@ -1,6 +1,6 @@
 import {add} from "./string_calc";
 
-describe('just testing', function() {
+describe('String calculator', function() {
     it('should return 0 for empty string', function() {
         expect(add("")).toEqual(0);
     });
@@ -15,5 +15,13 @@ describe('just testing', function() {
 
     it ('should sum arbitrary size of numbers', function() {
         expect(add("1,2,3,4,5,6,7,8,9")).toEqual(45);
-    })
+    });
+
+    it ('should cope with new line separator', function() {
+        expect(add("1\n2,3")).toEqual(6);
+    });
+
+    it ('should cope with custom separators', function() {
+        expect(add("//;\n1;2")).toEqual(3);
+    });
 });
