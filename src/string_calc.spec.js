78cb48761deb45a908d2ed9af2d663e1ca9e475e
Andy Smith <andysmith836@gmail.com>
2019-08-08 21:40:46 +0100

    prove we can sum lots of numbers

--- src/string_calc.spec.js
+++ src/string_calc.spec.js
@@ -12,4 +12,8 @@ describe('just testing', function() {
     it('should sum 2 numbers', function() {
         expect(add("1,2")).toEqual(3);
     });
+
+    it ('should sum arbitrary size of numbers', function() {
+        expect(add("1,2,3,4,5,6,7,8,9")).toEqual(45);
+    })
 });
