8257201ed5d6fae33cb0d7fc3047c6500861e674
Andy Smith <andysmith836@gmail.com>
2019-08-08 22:10:14 +0100

    Get error throwing working

--- src/string_calc.js
+++ src/string_calc.js
@@ -1,19 +1,33 @@
-function add(numbers) {
-    if (numbers === "") {
+function add(theString) {
+    if (theString === "") {
         return 0;
     }
 
+    const strings = splitValuesByDelimiter(theString);
+    let negatives = strings.map(s => parseInt(s)).
+        filter(i => i < 0);
+    if (negatives.length > 0) {
+        throw "error: negatives not allowed: -2 -3";
+    }
+
+    return sumValues(strings);
+}
+
+function splitValuesByDelimiter(theString) {
     let regExp = RegExp(/\/\/(.*)\n(.*)/);
-    let matches = regExp.exec(numbers);
+    let matches = regExp.exec(theString);
     let separator;
     if (matches !== null) {
         separator = matches[1];
-        numbers = matches[2];
+        theString = matches[2];
     } else {
         separator = /,|\n/
     }
 
-    let strings = numbers.split(separator);
+    return theString.split(separator);
+}
+
+function sumValues(strings) {
     return strings.map(s => parseInt(s))
         .reduce((total, acc) => total + acc);
 }
--- src/string_calc.spec.js
+++ src/string_calc.spec.js
@@ -1,27 +1,33 @@
 import {add} from "./string_calc";
 
 describe('String calculator', function() {
-    it('should return 0 for empty string', function() {
+    it('should return 0 for empty string', () => {
         expect(add("")).toEqual(0);
     });
 
-    it('should return 4 when passed in', function() {
+    it('should return 4 when passed in', () => {
         expect(add("4")).toEqual(4);
     });
 
-    it('should sum 2 numbers', function() {
+    it('should sum 2 numbers', () => {
         expect(add("1,2")).toEqual(3);
     });
 
-    it ('should sum arbitrary size of numbers', function() {
+    it ('should sum arbitrary size of numbers', () => {
         expect(add("1,2,3,4,5,6,7,8,9")).toEqual(45);
     });
 
-    it ('should cope with new line separator', function() {
+    it ('should cope with new line separator', () => {
         expect(add("1\n2,3")).toEqual(6);
     });
 
-    it ('should cope with custom separators', function() {
+    it ('should cope with custom separators', () => {
         expect(add("//;\n1;2")).toEqual(3);
+    });
+
+    it ('should disallow negatives', function() {
+        expect(() => {
+            add("1,-2,-3");
+        }).toThrowError("error: negatives not allowed: -2 -3");
     });
 });
